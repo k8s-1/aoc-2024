1e5b79da3aa3018166a19fdeb6dec9f92eaef455
pman <pman-$-432894328923@gmail.com>
2024-12-11 23:03:32 +0100

    ...

---
+++ 1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var l1 = [6]int{3, 4, 2, 1, 3, 3}
+	var l2 = [6]int{4, 3, 5, 3, 9, 3}
+
+	for _, v := range l1 {
+		fmt.Println(v)
+	}
+
+	for _, v := range l2 {
+		fmt.Println(v)
+	}
+}
