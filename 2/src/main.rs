71252f7e74149af5cb3f2714c4e88beace33817f
pman <pman-$-432894328923@gmail.com>
2024-12-12 08:18:42 +0100

    chore: update file(s): main.rs

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -9,22 +9,22 @@ fn main() -> io::Result<()> {
 
     let reader = io::BufReader::new(file);
 
-    let mut reports: Vec<Vec<i32>> = Vec::new();
+    let mut list_of_lists: Vec<Vec<i32>> = Vec::new();
 
     for line in reader.lines() {
         let line = line?;
-        let report: Vec<i32> = line
+        let numbers: Vec<i32> = line
             .split_whitespace()
             .filter_map(|s| s.parse::<i32>().ok())
             .collect();
-        reports.push(report);
+        list_of_lists.push(numbers);
     }
 
-    for report in &reports {
+    for list in &list_of_lists {
 
-        for i in 0..report.len() - 1 {
-            let current = report[i];
-            let next = report[i + 1];
+        for i in 0..numbers.len() - 1 { // Loop up to the second-to-last element
+            let current = numbers[i];
+            let next = numbers[i + 1];
 
             if current < next {
                 println!("{} is less than {}", current, next);
@@ -34,7 +34,6 @@ fn main() -> io::Result<()> {
                 println!("{} is equal to {}", current, next);
             }
         }
-
     }
 
     Ok(())
