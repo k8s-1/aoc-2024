8962965723505932309b95696ad697a2e67bd183
pman <pman-$-432894328923@gmail.com>
2024-12-12 08:13:10 +0100

    ...

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead};
+
+
+fn main() -> io::Result<()> {
+    let path = "input.txt";
+
+    let file = File::open(path)?;
+
+    let reader = io::BufReader::new(file);
+
+    let mut list_of_lists: Vec<Vec<i32>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let numbers: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+        list_of_lists.push(numbers);
+    }
+
+    for list in &list_of_lists {
+        println!("{:?}", list);
+    }
+
+    Ok(())
 }
